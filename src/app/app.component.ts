12593c779366b450e9d88c2ff261f5551ac0c063
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-11-24 14:26:34 -0500

    Reactive form setup
    section 15, lecture 201

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng8-forms-reactive';
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
 }
